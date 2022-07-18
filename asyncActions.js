55fcfca8a1d97641a439c40acf82753bd6e042de
shreyassp17 <shreyas@infomaticscorp.com>
2022-07-18 14:12:09 +0530

    Added comments

--- asyncActions.js
+++ asyncActions.js
@@ -1,81 +1,84 @@
-const redux = require('redux')
-const thunkMiddleware = require('redux-thunk').default
-const axios = require('axios')
-const createStore = redux.createStore
-const applyMiddleware = redux.applyMiddleware
+const redux = require("redux");
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
 
+//State
 const initialState = {
   loading: false,
   users: [],
-  error: ''
-}
+  error: "",
+};
 
-const FETCH_USERS_REQUESTED = 'FETCH_USERS_REQUESTED'
-const FETCH_USERS_SUCCEEDED = 'FETCH_USERS_SUCCEEDED'
-const FETCH_USERS_FAILED = 'FETCH_USERS_FAILED'
+//Actions
+const FETCH_USERS_REQUESTED = "FETCH_USERS_REQUESTED";
+const FETCH_USERS_SUCCEEDED = "FETCH_USERS_SUCCEEDED";
+const FETCH_USERS_FAILED = "FETCH_USERS_FAILED";
 
+//Action creators
 const fetchUsersRequest = () => {
   return {
-    type: FETCH_USERS_REQUESTED
-  }
-}
+    type: FETCH_USERS_REQUESTED,
+  };
+};
 
-const fetchUsersSuccess = users => {
+const fetchUsersSuccess = (users) => {
   return {
     type: FETCH_USERS_SUCCEEDED,
-    payload: users
-  }
-}
+    payload: users,
+  };
+};
 
-const fetchUsersFailure = error => {
+const fetchUsersFailure = (error) => {
   return {
     type: FETCH_USERS_FAILED,
-    payload: error
-  }
-}
+    payload: error,
+  };
+};
 
 const fetchUsers = () => {
   return function (dispatch) {
-    dispatch(fetchUsersRequest())
+    dispatch(fetchUsersRequest());
     axios
-      .get('https://jsonplaceholder.typicode.com/users')
-      .then(response => {
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
         // response.data is the users
-        const users = response.data.map(user => user.id)
-        dispatch(fetchUsersSuccess(users))
+        const users = response.data.map((user) => user.id);
+        dispatch(fetchUsersSuccess(users));
       })
-      .catch(error => {
+      .catch((error) => {
         // error.message is the error message
-        dispatch(fetchUsersFailure(error.message))
-      })
-  }
-}
+        dispatch(fetchUsersFailure(error.message));
+      });
+  };
+};
 
 const reducer = (state = initialState, action) => {
-  console.log(action.type)
+  console.log(action.type);
   switch (action.type) {
     case FETCH_USERS_REQUESTED:
       return {
         ...state,
-        loading: true
-      }
+        loading: true,
+      };
     case FETCH_USERS_SUCCEEDED:
       return {
         loading: false,
         users: action.payload,
-        error: ''
-      }
+        error: "",
+      };
     case FETCH_USERS_FAILED:
       return {
         loading: false,
         users: [],
-        error: action.payload
-      }
+        error: action.payload,
+      };
   }
-}
+};
 
-const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 store.subscribe(() => {
-  console.log(store.getState())
-})
-store.dispatch(fetchUsers())
+  console.log(store.getState());
+});
+store.dispatch(fetchUsers());
--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const redux = require("redux");
+
 const createStore = redux.createStore;
 const bindActionCreators = redux.bindActionCreators;
 const combineReducers = redux.combineReducers;
-const applyMiddleware =  redux.applyMiddleware;
-const reduxLogger = require('redux-logger')
-const logger = reduxLogger.createLogger()
+const applyMiddleware = redux.applyMiddleware;
+const reduxLogger = require("redux-logger");
+const logger = reduxLogger.createLogger();
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 const ICECREAM_ORDERED = "ICECREAM_ORDERED";
 const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
 
-function orderCake() {
+//Action Creator - returns an action
+function orderCake(qty = 1) {
   return {
     type: CAKE_ORDERED,
-    payload: 1,
+    payload: qty,
   };
 }
 
@@ -45,22 +47,24 @@ function restockIcecream(qty = 1) {
 //   // anotherProperty: 0
 // };
 
+//Initial states
 const initialCakeState = {
-  numOfCakes: 10
-}
+  numOfCakes: 10,
+};
 
 const initialIcecreamState = {
-  numOfIcecreams: 20
-}
+  numOfIcecreams: 20,
+};
 
 // (previousState, action) => newState
 
+//Reducers
 const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
-        ...state,
-        numOfCakes: state.numOfCakes - 1,
+        ...state, //Destructure the state and make a copy so that other properties inside it remain unchanged.
+        numOfCakes: state.numOfCakes - action.payload,
       };
 
     case CAKE_RESTOCKED:
@@ -75,7 +79,6 @@ const cakeReducer = (state = initialCakeState, action) => {
 };
 
 const icecreamReducer = (state = initialIcecreamState, action) => {
-
   switch (action.type) {
     case ICECREAM_ORDERED:
       return {
@@ -93,25 +96,28 @@ const icecreamReducer = (state = initialIcecreamState, action) => {
   }
 };
 
+//Combine the reducers
 const rootReducer = combineReducers({
   cake: cakeReducer,
-  iceCream: icecreamReducer
-})
+  iceCream: icecreamReducer,
+});
 
 const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("Initial state ", store.getState());
 
-const unsubscribe = store.subscribe(() =>{
+const unsubscribe = store.subscribe(() => {
   // console.log("Updated state ", store.getState())
-}
-);
+});
 
 // store.dispatch(orderCake())
 // store.dispatch(orderCake())
 // store.dispatch(orderCake())
 // store.dispatch(restockCake(2))
 
-const actions = bindActionCreators({ orderCake, restockCake, orderIcecream, restockIcecream }, store.dispatch);
+const actions = bindActionCreators(
+  { orderCake, restockCake, orderIcecream, restockIcecream },
+  store.dispatch
+);
 actions.orderCake();
 actions.orderCake();
 actions.orderCake();
